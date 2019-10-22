abcdbf8887a3198a46bd0ccd7d91519073f97a20
danielle lane <daniellelane1821@gmail.com>
2019-10-22 15:35:27 -0500

    fixed something

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,15 +9,18 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-            if (request.getMethod().equalsIgnoreCase("post")) {
-                String username = request.getParameter("username");
-                String password = request.getParameter("password");
-                if (username.equals("admin") && password.equals("password")) {
-                    response.sendRedirect("/profile");
-                }
-            }
-
             request.getRequestDispatcher("/login.jsp").forward(request, response);
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+                 if (request.getMethod().equalsIgnoreCase("post")) {
+                     String username = request.getParameter("username");
+                     String password = request.getParameter("password");
+                     if (username.equals("admin") && password.equals("password")) {
+                         response.sendRedirect("/profile");
+                     }
+                 }
     }
 }
